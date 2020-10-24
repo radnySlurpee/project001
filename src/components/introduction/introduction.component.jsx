9df2d393708b9730e976a7cfee4ce5431f9ddb12
unknown <radnyongtawco123@gmail.com>
2020-10-24 17:40:40 +0800

    minor adjustments

--- src/components/introduction/introduction.component.jsx
+++ src/components/introduction/introduction.component.jsx
@@ -8,7 +8,6 @@ import Col from 'react-bootstrap/Col';
 import { motion } from "framer-motion";
 
 import BaffleTextFilter from '../baffle-text-filter/baffle-text-filter';
-import {ReactComponent as Logo} from '../../assets/introduction_R.svg';
 
 
 const Introdution = () => {
@@ -25,24 +24,13 @@ const Introdution = () => {
                         ease: "easeOut"}}> 
 
                     <BaffleTextFilter 
-                    text={"TECHNOLOGY IS BEST WHEN IT BRINGS PEOPLE TOGETHER"} 
+                    text={"RAD"} 
                     setDuration={2000}
                     speed={150} 
                     obfuscate={false}
                     type={"tone"}/>
 
                 </motion.div>
-                <br/>
-                <motion.div
-                    initial={{ opacity: 0, y: -50 }}
-                    animate={{ opacity: 1, y: 0}}
-                    transition={{
-                        delay: 3,
-                        duration: 0.3,
-                        ease: "easeOut" }}>
-                            <Logo/>
-                </motion.div>
-                <br/><br/>
                 <motion.div 
                     className="arrow-down"
                     initial={{ opacity: 0, y: -40 }}
@@ -52,6 +40,7 @@ const Introdution = () => {
                         repeat: 3,
                         duration: 0.5,
                         ease: "linear" }}>
+                            . <br/>
                     <span>&#8595;</span>
                 </motion.div>
                 
